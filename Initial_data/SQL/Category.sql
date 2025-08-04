f320c27c6172ca5fbb45fdfe7795a3f883056100
AashishAdhikari27 <aashishadhikari2027@gmail.com>
2025-08-05 02:43:10 +0545

    Initial Data Loaded

---
+++ Initial_data/SQL/Category.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.shoppit_app_category (name,slug,image) VALUES
+	 ('Books','books','category_img/books_oxyUGyz.svg'),
+	 ('Clothes','clothes','category_img/clothing.svg'),
+	 ('Electronics','electronics','category_img/electronics.svg'),
+	 ('Foods','foods','category_img/food.svg'),
+	 ('Toys','toys','category_img/toy.svg');
